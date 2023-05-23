0498d3bd8fee2aa2c79f68d22d2be84d807a5452
Daniel Bristow <danbrist@gmail.com>
2023-05-23 12:46:12 +0200

    7th commit - routing reset

--- pages/contact.js
+++ pages/contact.js
@@ -5,18 +5,6 @@ import Head from 'next/head'
 
 export default () => (
   <div>
-    <Head>
-      <title>Contact</title>
-      {/* <!-- Google tag (gtag.js) --> */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-SS8PKG06Y1"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', 'G-SS8PKG06Y1');
-      </script>
-    </Head>
     <Layout>
       <h1>Contact me at:</h1>
       <Image src="../static/images/emailInsignia.jpg/" thumbnail />
