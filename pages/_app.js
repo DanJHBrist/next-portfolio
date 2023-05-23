8d1fd763052c4b8e60fd8530e34925f393aadba6
Daniel Bristow <danbrist@gmail.com>
2023-05-23 16:02:14 +0200

    add /app.js

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+
+import Script from 'next/script';
+
+function MyApp ({ Component, pageProps }){
+    return (
+        <>
+            <Script
+            strategy = 'afterInteractive'
+            src = {'https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT-PUBLIC-GOOGLE-ANALYTICS}'} 
+            />
+            
+            <Script strategy='afterInteractive'>
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+        
+                gtag('config', ${process.env.NEXT_GOOGLE_ANALYTICS});
+                `}
+            </Script>
+
+            <Component {...pageProps}/>    
+        </>
+    )
+}
+
+export default MyApp
